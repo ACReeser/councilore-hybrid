bcabbcc01662dfbee7acaacdcf7ef9cb78573ea0
Alex Reeser <rogueconformist@gmail.com>
2016-05-24 14:30:45 -0500

    Sample data start

--- src/app/models/decision.ts
+++ src/app/models/decision.ts
@@ -1,3 +1,36 @@
 export class Decision {
+    decisionID: string;
+    resolutionID: string;
+}
+
+export class DecisionResolution {
+    id: string;
+    preDescription: string;
+    postDescription: string;
+    specialInstructions: string; //might not be used
     
+    lawDelta: number = 0;
+    tradeDelta: number = 0;
+    farmingDelta: number = 0;
+    loreDelta: number = 0;
+    societyDelta: number = 0;
+    treasuryDelta: number = 0;
+    
+    lawRequirement: number = -10;
+    tradeRequirement: number = 0;
+    farmingRequirement: number = 0;
+    loreRequirement: number = 0;
+    societyRequirement: number = 0;
+    treasuryRequirement: number = 0;
+    
+    allowsExpansionPhase: boolean = true;
+    allowsRecruitmentPhase: boolean = true;
+}
+
+export class DailyDecision {
+    id: string;
+    title: string;
+    description: string;
+    requirements: Decision;
+    resolutions: DecisionResolution[] = [];
 }
