9fc9241a778855d5b2da4976a7705c3bd91066d6
Alex Reeser <rogueconformist@gmail.com>
2016-06-20 13:34:46 -0500

    static 'templates' for buildings
    I'm really not happy with how this looks. TS why can't i have multiple
    constructors or object instanciation shorthand!

--- src/app/models/building.ts
+++ src/app/models/building.ts
@@ -1,3 +1,4 @@
+import {Utils} from '../utils';
 export class Building {
     name: string;
     canBeUpgraded: boolean;
@@ -9,4 +10,26 @@ export class Building {
     loreDelta: number;
     societyDelta: number;
     lawDelta: number;
+    
+    constructor (aName: string, canUpgrade: boolean, image:string, statIcon:string, farmingD: number = 0, tradeD: number = 0, loreD: number = 0, societyD: number = 0, lawD: number = 0, cost:number = 5) {
+        this.name = aName;
+        this.canBeUpgraded = canUpgrade;
+        this.treasuryCost = cost;
+        this.imageSrc = image;
+        this.statIconSrc = statIcon;
+        this.farmingDelta = farmingD;
+        this.tradeDelta = tradeD;
+        this.loreDelta = loreD;
+        this.societyDelta = societyD;
+        this.lawDelta = lawD;
+    }
+    static construct(prototype: Building): Building{
+        return Utils._ExtendObject(prototype, {});    
+    }
+    
+    static Farm: Building = new Building("Farm", true, 'chapel', 'social', 2);
+    static Plantation: Building = new Building("Plantation", false, 'plantation', 'food', 3);
+    
+    static Chapel: Building = new Building("Chapel", true, 'chapel', 'social', 0, 0, 0, 2);
+    static Monastery: Building = new Building("Monastery", false, 'monastery', 'social', 0, 0, 0, 3);
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,4 +3,10 @@ export class Utils {
     public static _Extend<T>(targetClass: {new(...args : any[]): T;}, dumbObject: any): T{
         return <T>(<any>Object).setPrototypeOf(dumbObject, targetClass.prototype);
     }
+    public static _ExtendObject<T>(source: T, dumbObject: any): T{
+        for (var key in source) {
+            dumbObject[key] = source[key];
+        }
+        return <T>dumbObject;        
+    }
 }
